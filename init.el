c8e9375abdee746e56594268e7e058366ba1d320
ZHUO Qiang <zhuo.qiang@gmail.com>
2012-10-25 22:30:18 +0800

    init

---
+++ init.el
@@ -0,0 +1,251 @@
+;; -*- coding: utf-8 -*-
+;; -----------------------------------------------------------------------------
+;; ZHUO Qiang's Emacs Configuration 
+;; -----------------------------------------------------------------------------
+
+;; (setq debug-on-error t)
+
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+;; add load path
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory "~/.emacs.d/"
+    "default user emacs directory"))
+
+(defun qiang-in-emacs-directory (path)
+  (concat user-emacs-directory (convert-standard-filename path)))
+
+(let ((default-directory 
+        (qiang-in-emacs-directory "lisp")))
+  (normal-top-level-add-to-load-path '("."))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(load-library "qiang")
+
+(qiang-set-font
+ '("Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New") 
+ ":pixelsize=19"
+ '("Microsoft Yahei" "STHeiti" "华文黑体" "hei" 
+   "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+
+(setq text-scale-mode-step 1.1)
+;; For Linux
+(global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
+(global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
+;; For Windows
+(global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+(global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+
+
+(when (eq system-type 'darwin)
+  (setq delete-by-moving-to-trash t trash-directory "~/.Trash/emacs")
+  (setq mac-option-modifier 'hyper) ; Option key as Hyper
+  (setq mac-option-modifier 'super) ; Option key as Super
+  (setq mac-command-modifier 'meta) ; Command key as Meta
+  (setq mac-control-modifier 'ctrl) ; Control key as Ctrl
+  (setq mac-control-modifier 'ctrl) ; Control key as Ctrl
+  (setq ns-pop-up-frames nil); open file in current frame
+  )
+
+
+;; It is default now since Emacs 24
+;; (setq x-select-enable-clipboard t)
+;; (setq interprogram-paste-function 'x-selection-value)
+(if (functionp 'tool-bar-mode) (tool-bar-mode 0))
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(setq frame-title-format "%f %4 %b %Z %* %10 %I")
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(column-number-mode t)
+(setq display-time-day-and-date t)
+(display-time-mode t)
+(setq show-paren-style 'parentheses)
+(show-paren-mode t)
+(when window-system
+  (global-hl-line-mode t))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq x-stretch-cursor t) ; Stretch the cursor on TABs.
+(setq-default comint-process-echoes t) ;Do not echo the input in shell.
+(setq-default next-line-add-newlines nil)
+(setq visible-bell t)
+(setq enable-recursive-minibuffers t)
+;; (setq scroll-step 1
+;;       scroll-margin 5
+;;       scroll-conservatively 10000)
+
+;; Chinese sentence
+(setq sentence-end
+      "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*") 
+(setq sentence-end-double-space nil)
+
+(desktop-save-mode t)
+(setq desktop-dirname user-emacs-directory)
+(setq desktop-path `(,user-emacs-directory))
+(setq desktop-restore-eager 5)
+
+(if (functionp 'electric-pair-mode) (electric-pair-mode 1))
+
+(setq ido-save-directory-list-file (qiang-in-emacs-directory "ido.last"))
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-file-extensions-order
+      '(".org" ".txt" ".py" ".h" ".cpp" ".c" ".hpp" 
+        ".emacs" ".xml" ".el" ".ini" ".cfg" ".java"))
+(ido-mode t)
+(setq ido-ignore-extensions t)
+(setq ido-slow-ftp-host-regexps '(".*"))
+(setq ido-work-directory-list-ignore-regexps '(".*"))
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+
+(setq bookmark-save-flag 1)
+(setq kill-whole-line t)
+
+(global-set-key [(meta g)] 'goto-line)
+(global-set-key [(f5)] 'compile)
+(global-set-key (kbd "<C-f5>") 'revert-buffer)
+(global-set-key [(f8)] 'next-error)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "C-S-k") 'kill-whole-line)
+
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol)) 
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(icomplete-mode 1)
+
+(setq backup-directory-alist 
+      `((".*" . ,(qiang-in-emacs-directory "backup"))))
+(setq auto-save-file-name-transforms 
+      `((".*" ,(qiang-in-emacs-directory "auto-save") t)))
+
+(eval-after-load "compile"
+  '(progn
+     (setq-default compile-command "scons -u ")
+     (add-to-list 'compilation-error-regexp-alist 
+                  '("^ *File \"\\([^,\" \n\t]+\\)\", line \\([0-9]+\\)" 1 2))
+     (add-to-list 'compilation-error-regexp-alist 
+                  '("^\\(.*\\)(\\([0-9]+\\)):" 1 2))))
+
+(global-set-key "\M-;" 'qiang-comment-dwim-line)
+(global-set-key "\C-cR" 'qiang-rename-current-file-or-buffer)
+
+;; auto re-format when pasting code
+(dolist (command '(yank yank-pop))
+  (eval
+   `(defadvice, command (after indent-region activate)
+      (and (not current-prefix-arg)
+           (member major-mode
+                   '(emacs-lisp-mode lisp-mode clojure-mode scheme-mode
+                                     haskell-mode ruby-mode rspec-mode
+                                     c-mode c++-mode objc-mode
+                                     latex-mode js-mode plain-tex-mode))
+           (let ((mark-even-if-inactive transient-mark-mode))
+             (indent-region (region-beginning) (region-end) nil))))))
+
+(global-set-key (kbd "M-k") 'qiang-copy-line)
+
+(if window-system (server-mode 1))
+
+;; Emacs-List Mode
+(define-key emacs-lisp-mode-map [(f5)] 'eval-buffer)
+
+;; CC Mode
+(c-add-style
+ "qiang"
+ '("bsd"
+   (indent-tabs-mode . nil)
+   (c-basic-offset . 4)))
+
+(add-hook
+ 'c-mode-common-hook '
+ (lambda ()
+   (c-set-style "qiang")
+   (auto-fill-mode)
+   ;; (c-toggle-auto-hungry-state)
+   ))
+
+(setq
+ auto-mode-alist 
+ (union
+  '(("\\.\\(vsh\\|fsh\\|c\\)$" . c-mode)
+    ("\\.\\(cpp|\\cxx\\|h\\|hpp\\|hxx\\|cc\\|dox\\|mm\\|hmm\\)$" . c++-mode)
+    ("\\.\\(m\\|mm\\)$" . objc-mode))
+    auto-mode-alist))
+
+;; Java
+(add-hook
+ 'java-mode-hook '
+ (lambda ()
+   (c-set-style "java")
+   (auto-fill-mode)
+   (c-toggle-auto-hungry-state t)))
+
+(add-hook 'find-file-hook 'qiang-choose-header-mode)
+
+;; hexl Mode
+(setq auto-mode-alist
+      (cons '("\\.\\(exe\\|dll\\|so\\|o\\)$" . hexl-mode)
+            auto-mode-alist))
+
+
+;; python Mode
+(setq auto-mode-alist
+      (cons '("\\(\\.py?w\\)\\|\\(sconscript\\)\\|\\(sconstruct\\)\\|\\(SConstruct\\)\\|\\(SConscript\\)$" . python-mode)
+            auto-mode-alist))
+
+
+;; Lua Mode
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+
+;; Shell Mode
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+
+;; nXML Mode
+(setq auto-mode-alist
+      (cons '("\\(\\.xml\\)\\|\\(\\.html\\)|\\(\\.zcml\\)$" . nxml-mode)
+            auto-mode-alist))
+
+;; Markdown Mode
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+
+(require 'yasnippet) 
+(yas/global-mode 1)
+
+(require 'ahg)
+
+(require 'color-theme)
+(eval-after-load "color-theme"
+  (if window-system
+      '(progn
+         (color-theme-initialize)
+         (color-theme-blackboard))))
+
+(require 'quick-jump)
+(quick-jump-default-keybinding)
+
+(require 'breadcrumb)
+(global-set-key [(control f2)] 'bc-set)
+(global-set-key [(f2)] 'bc-previous)
+(global-set-key [(shift f2)] 'bc-next)
+(global-set-key [(meta f2)] 'bc-list)
---
+++ lisp/qiang.el
@@ -0,0 +1,119 @@
+(defun qiang-font-existsp (font)
+  (if (null (x-list-fonts font))
+      nil t))
+
+(defun qiang-make-font-string (font-name font-size)
+  (if (and (stringp font-size) 
+           (equal ":" (string (elt font-size 0))))
+      (format "%s%s" font-name font-size)
+    (format "%s %s" font-name font-size)))
+
+(defun qiang-set-font (english-fonts
+                       english-font-size
+                       chinese-fonts
+                       &optional chinese-font-size)
+  "english-font-size could be set to \":pixelsize=18\" or a integer.
+If set chinese-font-size to nil, it will follow english-font-size"
+  ;; 
+  ;; The following 2 method cannot make the new frame work.
+  ;; (set-default-font "Consolas:pixelsize=18")
+  ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+  ;; We will use set-face-attribute
+
+  (require 'cl)                         ; for find if
+  (if window-system
+      (let ((en-font (qiang-make-font-string
+                      (find-if #'qiang-font-existsp english-fonts)
+                      english-font-size))
+            (zh-font (font-spec :family 
+                                (find-if #'qiang-font-existsp chinese-fonts)
+                                :size chinese-font-size)))
+
+        (message "Set English Font to %s" en-font)
+        ;; Set English font
+        (set-face-attribute
+         'default nil :font en-font)
+        
+        (message "Set Chinese Font to %s" zh-font)
+        ;; Set Chinese font 
+        (dolist (charset '(kana han symbol cjk-misc bopomofo))
+          (set-fontset-font (frame-parameter nil 'font)
+                            charset
+                            zh-font)))
+    (message "Emacs in console will skip font setting.")))
+
+
+(defun qiang-comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line,
+        then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+(defun qiang-rename-current-file-or-buffer ()
+  (interactive)
+  (if (not (buffer-file-name))
+      (call-interactively 'rename-buffer)
+    (let ((file (buffer-file-name)))
+      (with-temp-buffer
+        (set-buffer (dired-noselect file))
+        (dired-do-rename)
+        (kill-buffer nil))))
+  nil)
+
+;; Smart copy, if no region active, it simply copy the current whole line
+(defadvice kill-line (before check-position activate)
+  (if (member major-mode
+              '(emacs-lisp-mode scheme-mode lisp-mode
+                                c-mode c++-mode objc-mode js-mode
+                                latex-mode plain-tex-mode))
+      (if (and (eolp) (not (bolp)))
+          (progn (forward-char 1)
+                 (just-one-space 0)
+                 (backward-char 1)))))
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end))
+                 (message "Copied line")
+                 (list (line-beginning-position)
+                       (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+
+;; Copy line from point to the end, exclude the line break
+(defun qiang-copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (point)
+                  (line-end-position))
+  ;; (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+
+
+(defun qiang-choose-header-mode ()
+  (interactive)
+  (if (string-equal (substring (buffer-file-name) -2) ".h")
+      (progn
+        ;; OK, we got a .h file, if a .m file exists we'll assume it's an objective c file.
+        ;; Otherwise, we'll look for a .cpp file.
+        (let ((dot-m-file (concat (substring (buffer-file-name) 0 -1) "m"))
+              (dot-cpp-file (concat (substring (buffer-file-name) 0 -1) "cpp")))
+          (if (file-exists-p dot-m-file)
+              (progn
+                (objc-mode))
+            (if (file-exists-p dot-cpp-file)
+                (c++-mode)))))))
+
+(defun qiang-system-name-is(name)
+  (interactive)
+  (string-equal (downcase system-name) name))

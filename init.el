f95f29eb21d7546721e6825603fb4d99df60faa4
ZHUO Qiang <zhuo.qiang@gmail.com>
2014-09-15 23:09:39 +0800

    add web development modes: coffeescript, jinja2, handlebars

--- init.el
+++ init.el
@@ -312,6 +312,24 @@
              (load "color-theme-solarized-dark")
              (color-theme-blackboard) (color-theme-calm-forest))))))
 
+(require 'jinja2-mode)
+
+(setq js-indent-level 2)
+
+;; coffeescript
+(require 'coffee-mode)
+(custom-set-variables
+ '(coffee-tab-width 2)
+ '(coffee-args-compile '("-c" "--bare")))
+
+(eval-after-load "coffee-mode"
+  '(progn
+     (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+     (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
+
+
+(require 'handlebars-mode)
+
 (require 'quick-jump)
 (quick-jump-default-keybinding)
 
